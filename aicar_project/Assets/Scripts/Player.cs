164eebcb09bfef8fa8a439b76a7be615ff7a5ced
이상락 <64376220+PangRAK@users.noreply.github.com>
2021-06-25 02:29:06 +0900

    신경망 구축

--- aicar_project/Assets/Scripts/Player.cs
+++ aicar_project/Assets/Scripts/Player.cs
@@ -25,20 +25,52 @@ public class Player : MonoBehaviour
     public RaycastHit2D hit4;
     public RaycastHit2D hit5;
 
+    // 인공지능 변수 세팅
+    public float[,] w1 = new float[4,5];    // 입력층 -> 은닉층 가중치
+    public float[,] w2 = new float[3,4];    // 은닉층 -> 출력층 가중치
+    public float[] b1 = new float[4];       // 은닉층의 편향
+    public float[] b2 = new float[3];       // 출력층의 편향
     
+    
+    System.Random rand = new System.Random();   // 난수 생성
+    public GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
         originPos = transform.position;
         sprite = gameObject.GetComponent<SpriteRenderer>();
 
+        float randomFloat = (float)rand.NextDouble();
+        // 첫번째 층 가중치, 편향 세팅
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                w1[i, j] = (float)rand.NextDouble();
+                //UnityEngine.Debug.Log("w1[" + i + "," + j + "] = " + w1[i,j]);
+            }
+            b1[i] = 0;
+        }
+        // 두번째 층 가중치, 편향 세팅
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                w2[i, j] = (float)rand.NextDouble();
+                //UnityEngine.Debug.Log("w2[" + i + "," + j + "] = " + w1[i, j]);
+            }
+            b2[i] = 0;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        AutoMove();
         CheckWall();
+        AutoMove();
     }
 
     void Move()
@@ -119,85 +151,86 @@ public class Player : MonoBehaviour
     void CheckWall()
     {
         // 센서 범위를 그려줌
-        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(0, 1)) * 20.0f, Color.red);
-        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(-0.2f, 1)) * 10.0f, Color.red);
-        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(0.2f, 1)) * 10.0f, Color.red);
-        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(-1, 1)) * 2.5f, Color.red);
-        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(1,1)) * 2.5f, Color.red);
+        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(0, 1)) * 10.0f, Color.red);
+        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(-0.3f, 1)) * 9.0f, Color.red);
+        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(0.3f, 1)) * 9.0f, Color.red);
+        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(-1, 1)) * 6.0f, Color.red);
+        UnityEngine.Debug.DrawRay(transform.position, transform.TransformDirection(new Vector2(1,1)) * 6.0f, Color.red);
 
         // 센서를 생성
-        hit1 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(0, 1)), 20.0f, LayerMask.GetMask("Wall"));
-        hit2 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(-0.2f, 1)), 10.0f, LayerMask.GetMask("Wall"));
-        hit3 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(0.2f, 1)), 10.0f, LayerMask.GetMask("Wall"));
-        hit4 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(-1, 1)), 3.5f, LayerMask.GetMask("Wall"));
-        hit5 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(1, 1)), 3.5f, LayerMask.GetMask("Wall"));
+        hit1 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(0, 1)), 10.0f, LayerMask.GetMask("Wall"));
+        hit2 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(-0.3f, 1)), 10.0f, LayerMask.GetMask("Wall"));
+        hit3 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(0.3f, 1)), 10.0f, LayerMask.GetMask("Wall"));
+        hit4 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(-1, 1)), 10.0f, LayerMask.GetMask("Wall"));
+        hit5 = Physics2D.Raycast(transform.position, transform.TransformDirection(new Vector2(1, 1)), 10.0f, LayerMask.GetMask("Wall"));
 
-        if (hit1.collider != null)
-        {
-            if (hit1.collider.gameObject.layer == 6)
-                UnityEngine.Debug.Log("hit1 = " + hit1.distance);
-        }
-        if (hit2.collider != null)
-        {
-            if (hit2.collider.gameObject.layer == 6)
-                UnityEngine.Debug.Log("hit2 = " + hit2.distance);
-        }
-        if (hit3.collider != null)
-        {
-            if (hit3.collider.gameObject.layer == 6)
-                UnityEngine.Debug.Log("hit3 = " + hit3.distance);
-        }
-        if (hit4.collider != null)
-        {
-            if (hit4.collider.gameObject.layer == 6)
-                UnityEngine.Debug.Log("hit4 = " + hit4.distance);
-        }
-        if (hit5.collider != null)
-        {
-            if (hit5.collider.gameObject.layer == 6)
-                UnityEngine.Debug.Log("hit5 = " + hit5.distance);
-        }
+        //if (hit1.collider != null)
+        //{
+        //    if (hit1.collider.gameObject.layer == 6)
+        //        UnityEngine.Debug.Log("hit1 = " + hit1.distance);
+        //}
+        //if (hit2.collider != null)
+        //{
+        //    if (hit2.collider.gameObject.layer == 6)
+        //        UnityEngine.Debug.Log("hit2 = " + hit2.distance);
+        //}
+        //if (hit3.collider != null)
+        //{
+        //    if (hit3.collider.gameObject.layer == 6)
+        //        UnityEngine.Debug.Log("hit3 = " + hit3.distance);
+        //}
+        //if (hit4.collider != null)
+        //{
+        //    if (hit4.collider.gameObject.layer == 6)
+        //        UnityEngine.Debug.Log("hit4 = " + hit4.distance);
+        //}
+        //if (hit5.collider != null)
+        //{
+        //    if (hit5.collider.gameObject.layer == 6)
+        //        UnityEngine.Debug.Log("hit5 = " + hit5.distance);
+        //}
 
     }
 
     void AutoMove()
     {
-        
         if (hit1.distance == 0)
-            hit1.distance = 9999f;
+            hit1.distance = 99f;
         if (hit2.distance == 0)
-            hit2.distance = 9999f;
+            hit2.distance = 99f;
         if (hit3.distance == 0)
-            hit3.distance = 9999f;
+            hit3.distance = 99f;
         if (hit4.distance == 0)
-            hit4.distance = 9999f;
+            hit4.distance = 99f;
         if (hit5.distance == 0)
-            hit5.distance = 9999f;
-
-        int random = UnityEngine.Random.Range(0, 10);
+            hit5.distance = 99f;
 
         speed_vec.y = speed;
         transform.Translate(speed_vec);
 
+        float[] r = new float[2];
+        r = Rak_Ai.predict(new float[] { hit1.distance, hit2.distance, hit3.distance, hit4.distance, hit5.distance }, w1, w2, b1, b2);
 
-        if (hit4.distance > hit5.distance)
-        {
-            Left();
-        }
-        else if(hit4.distance < hit5.distance)
-        {
-            Right();
-        }
+        //UnityEngine.Debug.Log("h1 = " + hit1.distance);
+        //UnityEngine.Debug.Log("h2 = " + hit2.distance);
+        //UnityEngine.Debug.Log("h3 = " + hit3.distance);
+        //UnityEngine.Debug.Log("h4 = " + hit4.distance);
+        //UnityEngine.Debug.Log("h5 = " + hit5.distance);
+        //UnityEngine.Debug.Log("r1 = " + r[0]);
+        //UnityEngine.Debug.Log("r2 = " + r[1]);
+
+        Left(r[0]);
+        Right(r[1]);
     }
 
-    void Left()
+    void Left(float degree)
     {
-        transform.Rotate(0, 0, Time.deltaTime * 200, Space.Self);
+        transform.Rotate(0, 0, Time.deltaTime * degree * 200, Space.Self);
     }
 
-    void Right()
+    void Right(float degree)
     {
-        transform.Rotate(0, 0, -Time.deltaTime * 200, Space.Self);
+        transform.Rotate(0, 0, -Time.deltaTime * degree * 200, Space.Self);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) // 두 물체간 물리적 충돌을 체크하고 싶다.
--- aicar_project/Assets/Scripts/RAK_AI.cs
+++ aicar_project/Assets/Scripts/RAK_AI.cs
@@ -8,16 +8,39 @@ namespace RAK_AI
 {
     class Rak_Ai
     {
-        static void Main(string[] args)
-        {
-        }
-
-
+        
         static public float sigmoid(float z)
         {
             return Convert.ToSingle(1 / (1 + Math.Exp(-z)));
         }
 
+        static public float[] softmax(float[] z)
+        {
+            return new float[] { z[0] / (z[0] + z[1]), z[1] / (z[0] + z[1]) };
+        }
 
+        static public float[] predict(float[] x, float[,] w1, float[,] w2, float[] b1, float[] b2)
+        {
+            float[] s1 = new float[4];
+            float[] s2 = new float[2];
+            float[] r1 = new float[4];
+            float[] r2 = new float[2];
+
+            // 입력층 -> 은닉층 계산
+            for (int i = 0; i < 4; i++)
+            {
+                s1[i] = (w1[i,0] * x[0]) + (w1[i,1] * x[1]) + (w1[i,2] * x[2]) + (w1[i,3] * x[3]) + (w1[i,4] * x[4]) + b1[i];
+                r1[i] = sigmoid(s1[i]);
+            }
+            // 은닉층 -> 출력층 계산
+            for (int i = 0; i < 2; i++)
+            {
+                s2[i] = (w2[i,0] * r1[0]) + (w2[i,1] * r1[1]) + (w2[i,2] * r1[2]) + (w2[i,3] * r1[3]) + b2[i];
+                
+            }
+            r2 = softmax(s2);
+
+            return r2;
+        }
     }
 }
