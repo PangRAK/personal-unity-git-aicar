ff99429aaa1113a6ea0ddd1fd5562843147ad2a3
이상락 <64376220+PangRAK@users.noreply.github.com>
2021-06-27 22:01:19 +0900

    complete uniform crossover
    균등 교차 연산 구현을 완료하였고 다음에는 변이를 구현해야한다.

--- aicar_project/Assets/Scripts/Main.cs
+++ aicar_project/Assets/Scripts/Main.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using UnityEngine;
 using RAK_AI;
+using System.Runtime.CompilerServices;
 
 public class Main : MonoBehaviour
 {
@@ -27,18 +28,20 @@ public class Main : MonoBehaviour
     public GameObject checkPoint7;
     public GameObject checkPoint8;
     public GameObject checkPoint9;
-    private Player playerScript1;
-    private Player playerScript2;
-    private Player playerScript3;
-    private Player playerScript4;
-    private Player playerScript5;
-    private Player playerScript6;
-    private Player playerScript7;
-    private Player playerScript8;
-    private Player playerScript9;
-    private Player playerScript10;
+    //private Player playerScript1;
+    //private Player playerScript2;
+    //private Player playerScript3;
+    //private Player playerScript4;
+    //private Player playerScript5;
+    //private Player playerScript6;
+    //private Player playerScript7;
+    //private Player playerScript8;
+    //private Player playerScript9;
+    //private Player playerScript10;
+    private Player[] playerScript = new Player[10];
     public float[] playerScore = new float[10];
     public float[] playerRank = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    System.Random rand = new System.Random();   // 난수 생성용
 
     // Start is called before the first frame update
     void Start()
@@ -66,16 +69,16 @@ public class Main : MonoBehaviour
         Thread.Sleep(20);
 
         // 10개의 자동차 각각의 Script 변수를 세팅함
-        playerScript1 = (Player)player1.GetComponent(typeof(Player));
-        playerScript2 = (Player)player2.GetComponent(typeof(Player));
-        playerScript3 = (Player)player3.GetComponent(typeof(Player));
-        playerScript4 = (Player)player4.GetComponent(typeof(Player));
-        playerScript5 = (Player)player5.GetComponent(typeof(Player));
-        playerScript6 = (Player)player6.GetComponent(typeof(Player));
-        playerScript7 = (Player)player7.GetComponent(typeof(Player));
-        playerScript8 = (Player)player8.GetComponent(typeof(Player));
-        playerScript9 = (Player)player9.GetComponent(typeof(Player));
-        playerScript10 = (Player)player10.GetComponent(typeof(Player));
+        playerScript[0] = (Player)player1.GetComponent(typeof(Player));
+        playerScript[1] = (Player)player2.GetComponent(typeof(Player));
+        playerScript[2] = (Player)player3.GetComponent(typeof(Player));
+        playerScript[3] = (Player)player4.GetComponent(typeof(Player));
+        playerScript[4] = (Player)player5.GetComponent(typeof(Player));
+        playerScript[5] = (Player)player6.GetComponent(typeof(Player));
+        playerScript[6] = (Player)player7.GetComponent(typeof(Player));
+        playerScript[7] = (Player)player8.GetComponent(typeof(Player));
+        playerScript[8] = (Player)player9.GetComponent(typeof(Player));
+        playerScript[9] = (Player)player10.GetComponent(typeof(Player));
 
     }
 
@@ -109,27 +112,24 @@ public class Main : MonoBehaviour
         //UnityEngine.Debug.Log("" + playerScript9.checkCount);
         //UnityEngine.Debug.Log("" + playerScript10.checkCount);
 
-        playerScore[0] = (playerScript1.checkCount * 100f) - CalcDist(player1, playerScript1.checkCount);
-        playerScore[1] = (playerScript2.checkCount * 100f) - CalcDist(player2, playerScript2.checkCount);
-        playerScore[2] = (playerScript3.checkCount * 100f) - CalcDist(player3, playerScript3.checkCount);
-        playerScore[3] = (playerScript4.checkCount * 100f) - CalcDist(player4, playerScript4.checkCount);
-        playerScore[4] = (playerScript5.checkCount * 100f) - CalcDist(player5, playerScript5.checkCount);
-        playerScore[5] = (playerScript6.checkCount * 100f) - CalcDist(player6, playerScript6.checkCount);
-        playerScore[6] = (playerScript7.checkCount * 100f) - CalcDist(player7, playerScript7.checkCount);
-        playerScore[7] = (playerScript8.checkCount * 100f) - CalcDist(player8, playerScript8.checkCount);
-        playerScore[8] = (playerScript9.checkCount * 100f) - CalcDist(player9, playerScript9.checkCount);
-        playerScore[9] = (playerScript10.checkCount * 100f) - CalcDist(player10, playerScript10.checkCount);
+        playerScore[0] = (playerScript[0].checkCount * 100f) - CalcDist(player1, playerScript[0].checkCount);
+        playerScore[1] = (playerScript[1].checkCount * 100f) - CalcDist(player2, playerScript[1].checkCount);
+        playerScore[2] = (playerScript[2].checkCount * 100f) - CalcDist(player3, playerScript[2].checkCount);
+        playerScore[3] = (playerScript[3].checkCount * 100f) - CalcDist(player4, playerScript[3].checkCount);
+        playerScore[4] = (playerScript[4].checkCount * 100f) - CalcDist(player5, playerScript[4].checkCount);
+        playerScore[5] = (playerScript[5].checkCount * 100f) - CalcDist(player6, playerScript[5].checkCount);
+        playerScore[6] = (playerScript[6].checkCount * 100f) - CalcDist(player7, playerScript[6].checkCount);
+        playerScore[7] = (playerScript[7].checkCount * 100f) - CalcDist(player8, playerScript[7].checkCount);
+        playerScore[8] = (playerScript[8].checkCount * 100f) - CalcDist(player9, playerScript[8].checkCount);
+        playerScore[9] = (playerScript[9].checkCount * 100f) - CalcDist(player10, playerScript[9].checkCount);
 
-        //for(int i = 0; i < 10; i++)
+        //for (int i = 0; i < 10; i++)
         //    UnityEngine.Debug.Log("score_" + (i + 1) + " = " + playerScore[i]);
 
-        //int rank = Array.IndexOf(playerScore, playerScore.Max()) + 1;
-        //UnityEngine.Debug.Log("rank = " + rank);
 
+        // 플레이어 순위 구하기
         for (int i = 0; i < playerScore.Length; i++)
             playerRank[i] = 1;
-
-        //[2] Process
         for (int i = 0; i < playerScore.Length; i++)
         {
             for (int j = 0; j < playerScore.Length; j++)
@@ -142,17 +142,10 @@ public class Main : MonoBehaviour
             }
         }
 
-        //for (int i = 0; i < playerScore.Length; i++)
-        //{
-        //    UnityEngine.Debug.Log("rank" + (i + 1) + " = " + playerRank[i]);
-        //}
-
-        int[] selection = new int[4];           // 골라진 4개
-        selection = Rak_Ai.rankingSelection();  // 랭킹셀렉션 실행
-        //for(int i = 0; i < selection.Length; i++)
-        //{
-        //    UnityEngine.Debug.Log("selection" + (i + 1) + " = " + selection[i]);
-        //}
+        for (int i = 0; i < playerScore.Length; i++)
+        {
+            UnityEngine.Debug.Log("rank" + (i + 1) + " = " + playerRank[i]);
+        }
     }
 
     float CalcDist(GameObject x_player, int count)
@@ -196,4 +189,118 @@ public class Main : MonoBehaviour
         }
         return result;
     }
+
+    void geneticProcess()
+    {
+        // 선택 알고리즘==============================================================
+        int[] selection = new int[4];           // 골라진 4개
+        selection = Rak_Ai.rankingSelection();  // 랭킹셀렉션 실행
+        //for(int i = 0; i < selection.Length; i++)
+        //{
+        //    UnityEngine.Debug.Log("selection" + (i + 1) + " = " + selection[i]);
+        //}
+
+        // 교차 알고리즘==============================================================
+        float[,,] selct_w1 = new float[4, 4, 5];    // 입력층 -> 은닉층 가중치
+        float[,,] selct_w2 = new float[4, 3, 4];    // 은닉층 -> 출력층 가중치
+
+        for (int i = 0; i < selection.Length; i++)  // 교차할 4명의의 유전자 저장
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                if(selection[i] == playerRank[j])
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        for (int y = 0; y < 5; y++)
+                        {
+                            selct_w1[i, x, y] = playerScript[j].w1[x, y];
+                        }
+                    }
+                }
+            }
+        }
+
+        // 교차 연산 시작 균등 교차를 사용
+        float[,,] child_w1 = new float[10, 4, 5];
+        float[,,] child_w2 = new float[10, 3, 4];
+
+        // 1번~5번 자식 1번,2번 부모를 이용하며 만듬
+        float randFloat = 0f;
+        for (int i = 0; i < 5; i++) // w1 가중치
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    randFloat = (float)rand.NextDouble();
+                    if (randFloat < 0.5f)
+                    {
+                        child_w1[i, x, y] = selct_w1[0, x, y];
+                    }
+                    else
+                    {
+                        child_w1[i, x, y] = selct_w1[1, x, y];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++) // w2 가중치
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    randFloat = (float)rand.NextDouble();
+                    if (randFloat < 0.5f)
+                    {
+                        child_w2[i, x, y] = selct_w2[0, x, y];
+                    }
+                    else
+                    {
+                        child_w2[i, x, y] = selct_w2[1, x, y];
+                    }
+                }
+            }
+        }
+
+        // 6번~10번 자식 3번,4번 부모를 이용하며 만듬
+        for (int i = 5; i < 10; i++) // w1 가중치
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    randFloat = (float)rand.NextDouble();
+                    if (randFloat < 0.5f)
+                    {
+                        child_w1[i, x, y] = selct_w1[2, x, y];
+                    }
+                    else
+                    {
+                        child_w1[i, x, y] = selct_w1[3, x, y];
+                    }
+                }
+            }
+        }
+        for (int i = 5; i < 10; i++) // w2 가중치
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    randFloat = (float)rand.NextDouble();
+                    if (randFloat < 0.5f)
+                    {
+                        child_w2[i, x, y] = selct_w2[2, x, y];
+                    }
+                    else
+                    {
+                        child_w2[i, x, y] = selct_w2[3, x, y];
+                    }
+                }
+            }
+        }
+    }
+
 }
